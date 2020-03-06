1e765e9298f93b5f3ccfb3dbfe87fbc2dc96f2ec
Tom Griffiths <tom@tomgriffithsphotos.com>
2020-03-06 11:25:50 +0000

    Updates

---
+++ precache-manifest.01b77b49663da783957bf393522ca20d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8255e61580b040823c79834b81fb55",
+    "url": "/bar-buddy/index.html"
+  },
+  {
+    "revision": "965f443bda4fa0866873",
+    "url": "/bar-buddy/static/css/main.8f651b1e.chunk.css"
+  },
+  {
+    "revision": "f2e70ac8e1c410696214",
+    "url": "/bar-buddy/static/js/2.b4975edc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/bar-buddy/static/js/2.b4975edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965f443bda4fa0866873",
+    "url": "/bar-buddy/static/js/main.794f59f1.chunk.js"
+  },
+  {
+    "revision": "1438d682ef94e68895b5",
+    "url": "/bar-buddy/static/js/runtime-main.fc0b0e17.js"
+  }
+]);
